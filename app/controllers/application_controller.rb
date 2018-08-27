5ec4aa1556e2a8201b10d67c10ac4d089065cf37
marcelo <espinoza.marcelo@live.com>
2018-08-27 20:30:27 -0300

    se añade eleccion de layout segun accion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,37 +1,31 @@
 class ApplicationController < ActionController::Base
-
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  rescue_from CanCan::AccessDenied do |exception|
+  layout :elegir_layout
 
+  rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_to root_url
-
   end
 
 
-  if @current_user == "client"
-     layout 'Panel'
+   
+  
+private
+
+  def elegir_layout
+    case action_name
+    when "new", "create"
+      "Panel"
+    when "index"
+      "Panel"
+    else
+      "Panel"
+    end
   end
-
-  if @current_user == "professional"
-      layout 'Profesional'
-  end
-
-  if @current_user == "admin"
-      layout 'Admin'
-  end
-
-  def users
-
-
-
-  end
-
-  protected
-
+end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre,:fecha_nacimiento , :numero_contacto , :direccion, :role])
   end
-end
+
