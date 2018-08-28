8fd86982b81fe1a4ad6f546bc2f6818a32928fa7
marcelo <espinoza.marcelo@live.com>
2018-08-28 20:06:10 -0300

    se añade usuario a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Specialty.create(name: "psicologia", institution: "UTEM" )
-Preexistence.create(name: "enfermedadx", description: "algo agloooo nooo" )
+Preexistence.create(name: "enfermedad1", description: "a continuacion se muestra las caracteristias" )
+user = User.create! :nombre => 'administrador', :email => 'a@admin.com', :password => '123456', :password_confirmation => '123456', :fecha_nacimiento => '1995-08-03', :direccion => 'calle falsa 123', :role => 'admin', :numero_contacto => '983551084' 
